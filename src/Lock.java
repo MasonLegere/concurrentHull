56e39eb4539e6ee6cde0808560ab5b01cee081cc
Mason Legere <masonlegere@gmail.com>
2020-03-23 07:33:24 -0700

    completed assignment

--- src/Lock.java
+++ src/Lock.java
@@ -1,3 +1,9 @@
+/**
+ * Implements a mutex lock using Lamport's Bakery Algorithm. This code has been rued from my
+ * operating systems assignment.
+ */
+
+
 public class Lock {
 
     private volatile int c[];
@@ -15,6 +21,11 @@ public class Lock {
 
     }
 
+    /**
+     * Obtains the mutex lock to thread with unique ID threadNum - may have to wait for other
+     * threads to finish their work in the critical section.
+     * @param threadNum The thread asking for the mutex lock
+     */
     public void lock(int threadNum){
 
         tk[threadNum] = true;
@@ -39,11 +50,19 @@ public class Lock {
 
     }
 
+    /**
+     * Drops the lock held by the thread with unique ID theadNum - done instantly.
+     * @param threadNum The thread asking for the mutex lock
+     */
     public void unlock(int threadNum){
       c[threadNum] = 0;
 
     }
 
+    /*
+    *  Returns the lowest priority index within the currently waiting threads. That is, the maximum
+    *  index. Newly created threads will be assigned this index plus one. 
+    * */
     public int maxID(){
         int max = c[0];
 
